9b6fe3bfa7a4abe1f9eb18048513952f68b821c0
Marcin Czachurski <mczachurski@icloud.com>
2023-05-29 15:36:19 +0200

    first commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+let package = Package(
+    name: "VernissageServer",
+    platforms: [
+       .macOS(.v12)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor")
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://www.swift.org/server/guides/building.html#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
+    ]
+)
