61d75d26a8f3460ff66bf787d774772f0da45e4e
Marcin Czachurski <mczachurski@icloud.com>
2023-07-29 17:03:21 +0200

    Fix SQL query

--- Sources/App/Services/UsersService.swift
+++ Sources/App/Services/UsersService.swift
@@ -476,10 +476,10 @@ final class UsersService: UsersServiceType {
         }
 
         try await sql.raw("""
-            UPDATE \(ident: User.schema)
-            SET followersCount = (SELECT count(1) FROM \(ident: Follow.schema) WHERE targetId = \(bind: userId)),
-                followingCount = (SELECT count(1) FROM \(ident: Follow.schema) WHERE sourceId = \(bind: userId))
-            WHERE id = \(bind: userId)
+            UPDATE "\(ident: User.schema)"
+            SET "followersCount" = (SELECT count(1) FROM "\(ident: Follow.schema)" WHERE targetId = \(bind: userId)),
+                "followingCount" = (SELECT count(1) FROM "\(ident: Follow.schema)" WHERE sourceId = \(bind: userId))
+            WHERE "id" = \(bind: userId)
         """).run()
     }
 }
