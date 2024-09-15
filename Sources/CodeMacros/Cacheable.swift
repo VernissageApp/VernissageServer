d3ce05fe44ab81d1c13fbad9e5f1e8b28096ab2d
Marcin Czachurski <mczachurski@icloud.com>
2024-09-15 13:01:19 +0200

    Add link to examples

--- Sources/CodeMacros/Cacheable.swift
+++ Sources/CodeMacros/Cacheable.swift
@@ -9,6 +9,8 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
+// Waiting for swift 6.0: https://github.com/swiftlang/swift-evolution/blob/main/proposals/0415-function-body-macros.md
 // @attached(body)
+
 @attached(peer, names: suffixed(_peer))
 public macro Cacheable(_ cacheKey: String? = nil) = #externalMacro(module: "CodeMacros", type: "CacheableMacro")
