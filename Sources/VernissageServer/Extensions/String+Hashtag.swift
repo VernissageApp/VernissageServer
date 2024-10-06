1a9b63d23115d3a20419972ff821dcac605675fc
Marcin Czachurski <mczachurski@icloud.com>
2024-10-06 11:54:54 +0200

    Fix hashtags with language accents (#134)

--- Sources/VernissageServer/Extensions/String+Hashtag.swift
+++ Sources/VernissageServer/Extensions/String+Hashtag.swift
@@ -8,7 +8,7 @@ import Foundation
 
 public extension String {
     func getHashtags() -> [String] {
-        let hashtagPattern = #/(?<tag>#+[a-zA-Z0-9(_)]{1,})/#
+        let hashtagPattern = #/(?<tag>#+[a-zA-Z\u00C0-\u024F\u1E00-\u1EFF\u0100-\u017F\u0180-\u024F0-9(_)]{1,})/#
         let matches = self.matches(of: hashtagPattern)
         
         let tags = matches.map { match in
