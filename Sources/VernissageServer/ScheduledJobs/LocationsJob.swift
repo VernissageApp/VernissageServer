d8d4b729503a7be2d600f7e2988aaaddcb692386
Marcin Czachurski <mczachurski@icloud.com>
2025-02-11 09:48:43 +0100

    #159 Move location migration to background job (#242)

---
+++ Sources/VernissageServer/ScheduledJobs/LocationsJob.swift
@@ -0,0 +1,29 @@
+//
+//  https://mczachurski.dev
+//  Copyright © 2024 Marcin Czachurski and the repository contributors.
+//  Licensed under the Apache License 2.0.
+//
+
+import Vapor
+import Fluent
+import Foundation
+import Queues
+
+/// A background task that fills the locations table.
+struct LocationsJob: AsyncScheduledJob {
+    let jobId = "LocationsJob"
+    
+    func run(context: QueueContext) async throws {
+        context.logger.info("LocationsJob is running.")
+
+        // Check if current job can perform the work.
+        guard try await self.single(jobId: self.jobId, on: context) else {
+            return
+        }
+
+        let locationsService = context.application.services.locationsService
+        try await locationsService.fill(on: context.executionContext)
+
+        context.logger.info("LocationsJob finished.")
+    }
+}
